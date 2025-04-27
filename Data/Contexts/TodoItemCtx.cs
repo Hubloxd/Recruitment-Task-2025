764549fa49a7c9b14d03c5038bc538f0e3bcb2ac
hubloxd <hjakobczuk@outlook.com>
2025-04-27 14:17:11 +0200

    Dodano implementacje modelu TodoItem

---
+++ Data/Contexts/TodoItemCtx.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recruitment_Task_2025.Data.Models;
+
+namespace Recruitment_Task_2025.Data.Contexts
+{
+    public class TodoItemCtx(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+    }
+}
---
+++ Data/Models/TodoItem.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Recruitment_Task_2025.Data.Models
+{
+    [Table("todo")]
+    public class TodoItem
+    {
+        [Key]
+        [Column("id")]
+        [Display(Name = "Todo Item Primary Key")]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "The Title's length must range between 3 and 100 characters")]
+        [Column("title")]
+        [Display(Name = "Todo Item Title")]
+        public required string Title { get; set; }
+
+        [StringLength(256)]
+        [Column("description")]
+        [Display(Name = "Todo Item Description")]
+        public string Description { get; set; } = string.Empty;
+
+        [Column("completion_percentage")]
+        [Display(Name = "Todo Item Completion Percentage")]
+        public ushort CompletionPercentage { get; set; } = 0;
+
+        [Column("created_at")]
+        [Display(Name = "Todo Item Creation Date")]
+        public DateTime CreatedAt { get; } = DateTime.Now;
+
+        [Column("expire_at")]
+        [Display(Name = "Todo Item Expiration Date")]
+        public DateTime ExpireAt { get; set; } = DateTime.Now.AddDays(7);
+    }
+}
