18c921c2f304ed683d831a298d82579a2b5c6712
hubloxd <hjakobczuk@outlook.com>
2025-04-28 16:47:02 +0200

    Dodano walidacje procentu ukonczenia i daty wygasniecia Todo

--- Data/DTOs/TodoItemDto.cs
+++ Data/DTOs/TodoItemDto.cs
@@ -16,12 +16,28 @@ namespace Recruitment_Task_2025.Data.DTOs
         [Display(Name = "Todo Item Description")]
         public string Description { get; set; } = string.Empty;
 
+        [Range(0, 100, ErrorMessage = "The Completion Percentage must be between 0 and 100")]
         [Column("completion_percentage")]
         [Display(Name = "Todo Item Completion Percentage")]
         public ushort CompletionPercentage { get; set; } = 0;
 
+        [NotMapped]
+        internal DateTime CreatedAt { get; } = DateTime.UtcNow;
+
         [Column("expire_at")]
         [Display(Name = "Todo Item Expiration Date")]
-        public DateTime ExpireAt { get; set; } = DateTime.Now.AddDays(7);
+        [CustomValidation(typeof(TodoItemDto), nameof(ValidateExpireAt))]
+        public DateTime ExpireAt { get; set; } = DateTime.UtcNow.AddDays(7);
+
+        public static ValidationResult? ValidateExpireAt(DateTime expireAt, ValidationContext context)
+        {
+            var todoItem = (TodoItemDto)context.ObjectInstance;
+
+            if (expireAt < todoItem.CreatedAt)
+            {
+                return new ValidationResult("The Expiration Date mustn't preceede the Creation Date");
+            }
+            return ValidationResult.Success;
+        }
     }
 }
